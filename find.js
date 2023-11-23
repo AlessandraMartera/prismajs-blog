ea25d9f3a7eae293ea00bfdd77a35ef98109abda
alessandra martera <ale.martera@gmail.com>
2023-11-23 09:56:59 +0100

    find only published post

--- find.js
+++ find.js
@@ -3,21 +3,33 @@ const prisma = new PrismaClient();
 
 
 // find del singolo post tramite slug
+// prisma.post
+//     .findUnique({
+//         where: {
+//             slug: "ricette-italiane"
+//         }
+//     })
+//     .then( post => {
+//         console.log("il post ricercato è:", post)
+//     } )
+//     .catch( error => console.error(error))
+
+// // find di tutti i post
+// prisma.post
+//     .findMany()
+//     .then( post => {
+//         console.log("tutti i post sono:", post)
+//     } )
+//     .catch( error => console.error(error))
+
+//  Crea una funzione che restituisca solo i Post pubblicati.
 prisma.post
-    .findUnique({
+    .findMany({
         where: {
-            slug: "ricette-italiane"
+            published: true
         }
     })
     .then( post => {
         console.log("il post ricercato è:", post)
-    } )
-    .catch( error => console.error(error))
-
-// find di tutti i post
-prisma.post
-    .findMany()
-    .then( post => {
-        console.log("tutti i post sono:", post)
     } )
     .catch( error => console.error(error))
